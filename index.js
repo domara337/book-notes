4d2b6ef38b348d8a8ef22e73d8448ad03d93518f
domara <domara.best@gmail.com>
2025-06-14 22:32:40 +0300

    inital commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import path from "path";
+
+
+
+const app=express();
+const port=3000;
+
+
+
+
+//middleware setup
+app.set('view engine' , 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+
+
+app.get("/insert", (req,res)=>{
+    res.render("create.ejs")
+})
+
+
+
+app.get("/", (req, res)=>{
+    res.render("index.ejs")
+})
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`server is running at http://localhost:${port}`)
+})
